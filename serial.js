fb35ee6670c5bda2755d874683235695fdd8f05d
Christopher de Beer <christopherdebeer@gmail.com>
2018-02-08 08:25:38 +0000

    simple web interface

--- serial.js
+++ serial.js
@@ -3,6 +3,7 @@
 
 const SerialPort = require('serialport')
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 var state = {
@@ -15,9 +16,12 @@ var state = {
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.json(state))
+app.get('/', (req, res) => res.sendFile('./public/index.html'))
+app.get('/data', (req, res) => res.send("<code>" + JSON.stringify(state, null, null, 4) + "</code>") )
 app.get('/runs/:runId', (req,res) => {
-	res.send("Show run [" + req.params.runId  + "] ...");	
+	fs.readFile( __dirname + '/logs/log-' + req.params.runId + '.csv' , 'utf8', function(err, contents) {
+		res.send( err ? err : contents);	
+	});
 })
 
 app.get('/toggle', (req, res) => {
@@ -26,12 +30,15 @@ app.get('/toggle', (req, res) => {
 	if (state.serialConnected) {
 		state.serialConnected = false;
 		state.serial.close( () => {
-			res.send("Completed /runs/" + state.runs[state.runs.length - 1])
+			// res.send("Completed /runs/" + state.runs[state.runs.length - 1])
+			res.redirect('/')
 			state.serial = null;
 		})
 	} else {
 		const ID = Date.now()
-		res.send("Started /runs/" + ID)
+
+		// res.send("Started /runs/" + ID)
+		res.redirect('/')
 
 		state.serialConnected = true
 		state.runs.push( ID )
@@ -39,11 +46,14 @@ app.get('/toggle', (req, res) => {
 			baudRate: 115200
 		})
 
-		var log = require('fs').createWriteStream( 'logs/log-' + ID + '.cvs');
+		var log = require('fs').createWriteStream( 'logs/log-' + ID + '.csv');
 
 		state.serial.on('data', (data) => {
 			console.log(data.toString());
-			log.write(Date.now() + ', ' + data)
+			if (data.indexOf(',') > -1) {
+				if (data.indexOf('heading') > -1 ) log.write( 'timestamp, ' + data)
+				else log.write(Date.now() + ', ' + data)
+			}
 		});
 
 		state.serial.on('error', (err) => {
