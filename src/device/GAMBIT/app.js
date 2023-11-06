9ea52c527dc692f88542eeab29e82995fdedcb80
Christopher de Beer <christopherdebeer@gmail.com>
2023-11-06 16:50:03 +0000

    use puck.js instead fo raw espruino.js

--- src/device/GAMBIT/app.js
+++ src/device/GAMBIT/app.js
@@ -1,3 +1,12 @@
+var log = [];
+var state = 1;
+
+function getData() {
+    for (var i=0;i<log.length;i++)
+        console.log(i+","+log[i]);
+}
+
+
 //NFC Detection
 NRF.nfcURL("webble://christopherdebeer.github.io/simcap/src/web/GAMBIT/");
 NRF.on('NFCon', function() {
@@ -13,4 +22,45 @@ NRF.on('NFCoff', function() {
     NRF.setAdvertising({
         0x183e: [0],
     });
+});
+
+//Movement Sensor
+require("puckjsv2-accel-movement").on();
+var idleTimeout;
+Puck.on('accel',function(a) {
+    // digitalWrite(LED1,1); //turn on red light
+  if (idleTimeout) clearTimeout(idleTimeout);
+  else
+    if (state === 1) {
+        console.log('movement : 1');
+        log.push(`a:1}`)
+        NRF.setAdvertising({
+            0x182e: [1],
+        });
+    }
+    idleTimeout = setTimeout(function() {
+        idleTimeout = undefined;
+        // digitalWrite(LED1,0);//turn off red light
+        if (state === 1) {
+            console.log('movement : 0');
+            log.push(`a:0}`)
+            NRF.setAdvertising({
+                0x182e: [0],
+            });
+        }
+    },500);  
+});
+
+
+//Magnetic Field Sensor
+require("puckjsv2-mag-level").on();
+Puck.on('field',function(m) {
+    digitalPulse(LED2, 1, 200);//flash green light
+    if (state === 1) {
+        console.log('magnetic_field : [' + m.state + ']');
+        NRF.setAdvertising({
+            0x183a: [m.state],
+        });
+        log.push(`m:${m.state}`)
+    }
 });
