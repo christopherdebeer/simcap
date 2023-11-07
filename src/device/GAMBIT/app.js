0dc4337b425cbcd92ca29cbcc8775968ac1533ed
Christopher de Beer <christopherdebeer@gmail.com>
2023-11-07 11:45:26 +0000

    Improve data capture for GAMBIT

--- src/device/GAMBIT/app.js
+++ src/device/GAMBIT/app.js
@@ -1,9 +1,28 @@
-var log = [];
 var state = 1;
 
+var telemetry = {
+    a: null,
+    g: null,
+    m: null,
+    l: null,
+    t: null,
+    c: null,
+    s: state,
+    b: null,
+}
+
 function getData() {
-    for (var i=0;i<log.length;i++)
-        console.log(i+","+log[i]);
+    telemetry.m = Puck.mag()
+    var accel = Puck.accel()
+    telemetry.a = accel.acc
+    telemetry.g = accel.gyro
+    telemetry.l = Puck.light()
+    telemetry.t = Puck.magTemp()
+    telemetry.c = Puck.capSense()
+    telemetry.s = state
+    telemetry.b = Puck.getBatteryPercentage()
+    console.log("\nGAMBIT" + JSON.stringify(telemetry))
+    return telemetry;
 }
 
 
@@ -24,46 +43,43 @@ NRF.on('NFCoff', function() {
     });
 });
 
-//Movement Sensor
-require("puckjsv2-accel-movement").on();
-var idleTimeout;
-Puck.on('accel',function(a) {
-    // digitalWrite(LED1,1); //turn on red light
-  if (idleTimeout) clearTimeout(idleTimeout);
-  else
-    if (state === 1) {
-        console.log('movement : 1');
-        log.push(`a:1}`)
-        NRF.setAdvertising({
-            0x182e: [1],
-        });
-    }
-    idleTimeout = setTimeout(function() {
-        idleTimeout = undefined;
-        // digitalWrite(LED1,0);//turn off red light
-        if (state === 1) {
-            console.log('movement : 0');
-            log.push(`a:0}`)
-            NRF.setAdvertising({
-                0x182e: [0],
-            });
-        }
-    },500);  
-});
+// //Movement Sensor
+// require("puckjsv2-accel-movement").on();
+// var idleTimeout;
+// Puck.on('accel',function(a) {
+//     // digitalWrite(LED1,1); //turn on red light
+//   if (idleTimeout) clearTimeout(idleTimeout);
+//   else
+//     if (state === 1) {
+//         console.log('movement : 1');
+//         NRF.setAdvertising({
+//             0x182e: [1],
+//         });
+//     }
+//     idleTimeout = setTimeout(function() {
+//         idleTimeout = undefined;
+//         // digitalWrite(LED1,0);//turn off red light
+//         if (state === 1) {
+//             console.log('movement : 0');
+//             NRF.setAdvertising({
+//                 0x182e: [0],
+//             });
+//         }
+//     },500);  
+// });
 
 
-//Magnetic Field Sensor
-require("puckjsv2-mag-level").on();
-Puck.on('field',function(m) {
-    digitalPulse(LED2, 1, 200);//flash green light
-    if (state === 1) {
-        console.log('magnetic_field : [' + m.state + ']');
-        NRF.setAdvertising({
-            0x183a: [m.state],
-        });
-        log.push(`m:${m.state}`)
-    }
-});
+// //Magnetic Field Sensor
+// require("puckjsv2-mag-level").on();
+// Puck.on('field',function(m) {
+//     digitalPulse(LED2, 1, 200);//flash green light
+//     if (state === 1) {
+//         console.log('magnetic_field : [' + m.state + ']');
+//         NRF.setAdvertising({
+//             0x183a: [m.state],
+//         });
+//     }
+// });
 
 //Button Press
 //Turn Off/On MQTT Advertising
@@ -74,11 +90,12 @@ setWatch(function() {
     if ((pressCount+1)%2) digitalPulse(LED3,1,1500); //long flash blue light
     else
         digitalPulse(LED3,1,100); //short flash blue light
-    console.log('button_press_count : [' + pressCount + ']');
-    console.log('button_state : [' + (pressCount+1) + ']');
-    console.log('state: ' + state); 
-    NRF.setAdvertising({
-        0xFFFF : [pressCount],
-        0x183c: [((pressCount+1)%2)],
-    });
+    getData()
+    // console.log('button_press_count : [' + pressCount + ']');
+    // console.log('button_state : [' + (pressCount+1) + ']');
+    // console.log('state: ' + state); 
+    // NRF.setAdvertising({
+    //     0xFFFF : [pressCount],
+    //     0x183c: [((pressCount+1)%2)],
+    // });
 }, BTN, { edge:"rising", repeat:true, debounce:50 });
